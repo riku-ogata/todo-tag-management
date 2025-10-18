43db765e052290ef73cd048e056bf875db39c408
R.Ogata <r.ogata@ROgatanoMacBook-Air.local>
2025-10-18 17:58:50 +0900

    initial commit: Profect stryctyre setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Todo Tag Manager</h1>
+        <p>Welcome to your todo application!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
